847cdb7fa6dcea7edcd11117e31c1ce70a6a8f40
raissas2 <raissa.floriano@aluno.senai.br>
2023-08-29 16:51:11 -0300

    feat:finalizacao do html e js

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -6,20 +6,13 @@ function verificarInputs(){
     let especie = document.getElementById("input-especie").value;
     let fotolink = document.getElementById("input-fotolink").value;
     let date = document.getElementById("input-date").value;
-
-    console.log({tutor});
-    console.log({nomepet});
-    console.log({especie});
-    console.log({fotolink});
-    console.log({date});
-
 if(tutor == "" || nomepet == "" || especie == "" || fotolink == "" || date == ""){
     console.log("Os campos estão vazios!");
     envieMsg("Preencha todos os campos!", "erro");
-   
+   return true
 } else {
     console.log("Os campos não estão em brancos!");
-   
+    return false
    }
 }
 
@@ -44,6 +37,19 @@ class Pets{
         this.especie = especie;
         this.fotolink = fotolink;
         this.date = date;
+        this.idade = this.getAge();
+    }
+    getAge(){
+        let today = new Date();
+        let birthdate = new Date(this.date);
+        let age = today.getFullYear() - birthdate.getFullYear();
+        let month = today.getMonth() - birthdate.getMonth();
+
+        if (month < 0 || (month === 0 && today.getDate() < birthdate.getDate())) {
+            age--;
+        }
+        return age;
+        
     }
 }
 
@@ -60,6 +66,7 @@ function registrePets(){
     const pets = new Pets(tutor,nomepet,especie,fotolink,date);
     console.log(pets);
     bibliotecadepets.add(pets)
+    renderizarConteudo();
 }
 
 class ListadePets{
@@ -70,6 +77,8 @@ class ListadePets{
 add(parametro){
     if(verificarInputs()){
         envieMsg("Preencha todos os campos!", "erro");
+    } else if(!isURLValida(parametro.fotolink)) {
+        envieMsg("URL inválida", "erro")
     }else{
         this.listadepets.push(parametro);
         limparInputs()
@@ -91,4 +100,47 @@ function limparInputs(){
     let date = document.getElementById("input-date").value = "";
 }
 
+function renderizarConteudo() {
 
+    const listaHtml = document.getElementById('containerLista');
+    listaHtml.innerHTML = '';
+    let array = bibliotecadepets.listadepets;
+    console.log(array)
+    array.forEach(pets => {
+        const petsDiv = ` 
+         <div id='Petsclinic'>
+         <p>Tutor: ${pets.tutor}</p>
+         <p>Nome do Pet: ${pets.nomepet}</p>
+         <p>Espécie: ${pets.especie}</p>
+         <img src="${pets.fotolink}" alt="${pets.tutor}">
+         <p>Data de Nascimento: ${dateinPTBR(pets.date)}</p>
+         <p>Idade: ${pets.idade}</p>
+
+        </div>
+        `;
+        listaHtml.innerHTML += petsDiv;
+    });
+}
+function isURLValida(url) {
+    if(url.match(/\.(jpeg|jpg|gif|png)$/) != null){
+        return true;
+    } else {
+        return false;
+    }
+}
+function dateinPTBR(data) {
+    let dateArray = data.split("-");
+    let datePTBR = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
+    return datePTBR;
+}
+function RegistroPets(){
+    document.getElementById("principal").classList.remove("hidden");
+    document.getElementById("containerLista").classList.add("hidden");
+    document.getElementById("Petsclinic").classList.add("hidden");
+}
+
+function ExibicaoPets(){
+    document.getElementById("principal").classList.add("hidden");
+    document.getElementById("containerLista").classList.remove("hidden");
+    document.getElementById("Petsclinic").classList.remove("hidden");
+}
