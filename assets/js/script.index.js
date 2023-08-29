cb2ee68c8daf5a8a6aee7ca08d672790abb64cf4
raissas2 <raissa.floriano@aluno.senai.br>
2023-08-29 10:09:24 -0300

    feat: função limpar inputs

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -37,3 +37,58 @@ let msgnaTela = `
    }, 3000);
 }
 
+class Pets{
+    constructor(tutor,nomepet,especie,fotolink,date){
+        this.tutor = tutor;
+        this.nomepet = nomepet;
+        this.especie = especie;
+        this.fotolink = fotolink;
+        this.date = date;
+    }
+}
+
+const petsclinic = new Pets ("Raissa", "Revoada", "Cachorro", "link", "01-01-23")
+console.log(petsclinic);
+
+function registrePets(){
+    let tutor = document.getElementById("input-tutor").value;
+    let nomepet = document.getElementById("input-nomepet").value;
+    let especie = document.getElementById("input-especie").value;
+    let fotolink = document.getElementById("input-fotolink").value;
+    let date = document.getElementById("input-date").value;
+
+    const pets = new Pets(tutor,nomepet,especie,fotolink,date);
+    console.log(pets);
+    bibliotecadepets.add(pets)
+}
+
+class ListadePets{
+    constructor(){
+        this.listadepets = [];
+    }
+
+add(parametro){
+    if(verificarInputs()){
+        envieMsg("Preencha todos os campos!", "erro");
+    }else{
+        this.listadepets.push(parametro);
+        limparInputs()
+    envieMsg("cadastrado com sucesso!","sucesso")
+    console.log(this.listadepets);
+    }
+
+    }
+}
+
+const bibliotecadepets = new ListadePets();
+console.log(bibliotecadepets);
+
+function limparInputs(){
+    let tutor = document.getElementById("input-tutor").value = "";
+    let nomepet = document.getElementById("input-nomepet").value = "";
+    let especie = document.getElementById("input-especie").value = "";
+    let fotolink = document.getElementById("input-fotolink").value = "";
+    let date = document.getElementById("input-date").value = "";
+}
+
+
